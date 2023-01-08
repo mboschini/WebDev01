7a45b59d57acd5f2ef453200edd809ef5f6afba2
mboschini <boschini.mariano@gmail.com>
2023-01-08 17:51:48 -0300

    Finished cv html+css+js v1

---
+++ js/cvProperties.js
@@ -0,0 +1,24 @@
+const apiweather = 
+{
+    key:"ac7c8753adc5ffc5434b73fba668a1a0",
+    url: "https://api.openweathermap.org/data/2.5/weather"
+}
+
+const weatherText = document.getElementById("temp-text");
+
+async function search()
+{
+    try 
+    {
+        const response = await fetch(`${apiweather.url}?lat=-34.57&lon=-58.49&appid=${apiweather.key}&units=metric`);
+        const data = await response.json();   
+        weatherText.innerHTML = `${"Actual Weather: " + data.weather[0].description + " "+ data.main.temp.toFixed(1) + "°c"}`;
+    } 
+    catch (error) 
+    {
+        console.log(error);
+        alert("hubo un error");
+    }
+
+}
+search();
